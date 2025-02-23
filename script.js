ff98216c64621f4cc286aa23ab1badd00d241dcf
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-02-23 16:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function daysOfAYear(n){
+	if(n%400===0)return 366
+	if(n%100===0)return 365
+	if(n%4)return 366
+	return 365
+}
+year=prompt()
+console.log(daysOfAYear(year))
